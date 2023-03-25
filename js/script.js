59cf3f040ac1e3ca5bb191e4ac1849120c51b2c1
ihumph0228 <128190629+ihumph0228@users.noreply.github.com>
2023-03-25 17:21:04 -0500

    Reorganized code
    broke apart sections of the array to make it easier to read.
    
    changed text in to create for loop that displays the pokemon and the height.
    
    created special message if heght is over 50" (applies only to one"

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 //Array of Pokemon. Height is listed as inches
 let pokemonList = [
-    { name: 'Blastoise', height: "63\"", type: ['Water'] },
-    { name: 'Butterfree', height: "43\"", type: ['Bug, Flying'] },
-    { name: 'Pikachu', height: "16\"", type: ['Electric'] }
+    {
+      name: 'Blastoise', 
+      height: "63\"", 
+      type: ['Water']
+    },
+    { 
+      name: 'Butterfree', 
+      height: "43\"", 
+      type: ['Bug, Flying']
+    },
+    { 
+      name: 'Pikachu', 
+      height: "16\"", 
+      type: ['Electric'] 
+    }
 ];
 
-// Get the <ul> elemnt by its ID
-let ul = document.getElementById('pokemonList');
-
 // Loop through the pokemonList array to creat an <li> element for each Pokemon
-pokemonList.forEach(pokemon => {
-    let li = document.createElement('li');
-    li.textContent = `${pokemon.name} (height: ${pokemon.height}, type: ${pokemon.type.join(', ')})`;
-    ul.appendChild(li);
-});
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemonText = '<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')';
+    
+    // Add a message if the Pokemon's height is greater than 50 inches
+    if (parseFloat(pokemonList[i].height) > 50) {
+        pokemonText += ' wow, that is big!';
+    }
+
+    pokemonText += '</p>';
+    document.write(pokemonText);
+}
