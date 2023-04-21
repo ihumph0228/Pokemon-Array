38b1766816c181b376a59216786d9bb07f016e4b
ihumph0228 <128190629+ihumph0228@users.noreply.github.com>
2023-04-21 12:16:46 -0500

    Moved most functions around and tried to make it work correctly

--- js/script.js
+++ js/script.js
@@ -1,125 +1,81 @@
-// Polyfills for promises and fetch
-if (!window.Promise) {
-  window.Promise = Promise;
-}
+// Define the pokemonRepository object with methods and properties
+var pokemonRepository = (function() {
+  var pokemonList = []; // Array to store the Pokémon data
 
-if (!window.fetch) {
-  window.fetch = function(url) {
-    return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          resolve(xhr.responseText);
-        } else {
-          reject(Error(xhr.statusText));
-        }
-      };
-      xhr.onerror = function() {
-        reject(Error('Network Error'));
-      };
-      xhr.send();
-    });
-  };
-}
+  // Function to add Pokémon data to the pokemonList array
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-let pokemonRepository = (function() {
-  // The array of Pokemon objects
-  let pokemonList = [];
-
-  // Returns the entire pokemonList array
+  // Function to get all Pokémon data from the pokemonList array
   function getAll() {
     return pokemonList;
   }
 
-  // Adds a new Pokemon object to the end of the pokemonList array
-  function add(item) {
-    pokemonList.push(item);
-  }
-
-  // Creates an li element with a button inside and appends it to the .pokemon-list ul in index.html
-  function addListItem(pokemon) {
-    // Select the unordered list in index.html
-    let pokemonList = document.querySelector('#pokemon-list');
-
-    // Create a new list item element
-    let listItem = document.createElement('li');
-
-    // Append the list item to the unordered list
-    pokemonList.appendChild(listItem);
-
-    // Create a new button element
-    let button = document.createElement('button');
-
-    // Set the button's text to the Pokemon's name
-    button.innerText = pokemon.name;
-
-    // Add a custom class to the button for styling
-    button.classList.add('button-class');
-
-    // Append the button to the list item
-    listItem.appendChild(button);
-    
-    //Add event listener to the button
-    button.addEventListener('click', function() {
-      showDetails(pokemon);
-    });
-  }
-
-  // Creates a new function to log the Pokemon details
-  function showDetails(pokemon) {
-    // Call the loadDetails() function with the Pokemon object as parameter
-    loadDetails(pokemon)
-      .then(function(details) {
-        // Log the Pokemon details in the console
-        console.log(details);
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
-  }
-
-  // Load data from external source
-  function LoadList() {
-    return fetch('https://pokeapi.co/api/v2/pokemon/')
+  // Fetch Pokémon data from the API and call the add() function to populate the pokemonList array
+  function loadList() {
+    return fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
       .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        data.results.forEach(function(pokemon) {
-          // Call the add() function to add each Pokemon from the results to the pokemonList variable
-          add({
-            name: pokemon.name,
-            detailsUrl: pokemon.url
-          });
+        data.results.forEach(function(item) {
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
         });
       })
       .catch(function(error) {
-        console.error(error);
+        console.error('Failed to load Pokémon list', error);
       });
   }
 
-  // Load Pokemon details from external source
-  function loadDetails(pokemon) {
-    return fetch(pokemon.detailsUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        // Assign some of the details from the response to the Pokemon in the pokemonList
-        pokemon.imgUrl = data.sprites.front_default;
-        pokemon.height = data.height;
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
-  }
-
-  // Return an object containing the public functions
+  // Return the public methods and properties
   return {
-    getAll: getAll,
     add: add,
-    addListItem: addListItem,
-    LoadList: LoadList,
-    loadDetails: loadDetails
- 
+    getAll: getAll,
+    loadList: loadList
+  };
+})();
+
+// Function to render the Pokémon list in the HTML
+function renderPokemonList() {
+  var pokemonListElement = document.getElementById('pokemon-list');
+
+  // Clear the existing content of the list
+  pokemonListElement.innerHTML = '';
+
+  // Loop through the pokemonList array and create list items for each Pokémon
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    var listItemElement = document.createElement('li');
+    var linkElement = document.createElement('a');
+    linkElement.href = pokemon.detailsUrl;
+    linkElement.textContent = pokemon.name;
+
+    // Add click event listener on each list item
+    listItemElement.addEventListener('click', function() {
+      // Fetch the Pokémon details from the API
+      fetch(pokemon.detailsUrl)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data); // Display the Pokémon object in the console
+        })
+        .catch(function(error) {
+          console.error('Failed to load Pokémon details', error);
+        });
+    });
+
+    listItemElement.appendChild(linkElement);
+    pokemonListElement.appendChild(listItemElement);
+  });
+}
+
+// Call the loadList() function to fetch Pokémon data from the API and populate the pokemonList array
+pokemonRepository.loadList().then(function() {
+  // Call the renderPokemonList() function to render the Pokémon list in the HTML
+  renderPokemonList();
+});
