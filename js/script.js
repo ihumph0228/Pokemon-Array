c2ca23f7072f4f3b2888b8a8141b2b8ed41d7dff
ihumph0228 <128190629+ihumph0228@users.noreply.github.com>
2023-05-11 18:14:44 -0500

    Trying to fix.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,75 @@
 $(document).ready(function() {
-  var pokemonList = $(".pokemon-list");
+  // API URL and default values
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  let limit = 10;
+  let offset = 0;
 
-  // Fetch the list of Pokemons from the API
-  $.get("https://pokeapi.co/api/v2/pokemon/?limit=150").done(function(data) {
-    var pokemons = data.results;
+  // Fetch Pokemon data
+  function fetchPokemon() {
+      $.ajax({
+          url: `${apiUrl}?limit=${limit}&offset=${offset}`,
+          method: 'GET'
+      }).done(function(data) {
+          const pokemonList = $('.pokemon-list');
+          pokemonList.empty(); // Clear existing list items
 
-    // Loop through each Pokemon and create a list item for it
-    $.each(pokemons, function(index, pokemon) {
-      var listItem = $("<li>").addClass("list-group-item");
-      var link = $("<a>").attr("href", "#").text(pokemon.name);
-      listItem.append(link);
-      pokemonList.append(listItem);
+          // Loop through the returned data and add each Pokemon to the list
+          $.each(data.results, function(index, pokemon) {
+              const listItem = $('<li class="list-group-item"></li>');
+              const pokemonLink = $('<a></a>');
+              pokemonLink.text(pokemon.name);
+              pokemonLink.attr('href', '#');
+              pokemonLink.click(function() {
+                  // Display Pokemon details modal when clicked
+                  displayPokemonDetails(`${apiUrl}${pokemon.name}`);
+              });
+              pokemonLink.appendTo(listItem);
+              listItem.appendTo(pokemonList);
+          });
 
-      // Attach a click event listener to each list item
-      listItem.click(function() {
-        // Fetch details for the clicked Pokemon
-        $.get(pokemon.url).done(function(details) {
-          // Set the modal title and body
-          $(".modal-title").text(details.name);
-          var body = $("<div>").addClass("pokemon-details");
-          body.append($("<p>").text("Height: " + details.height));
-          body.append($("<p>").text("Weight: " + details.weight));
-          body.append($("<img>").attr("src", details.sprites.front_default));
-          $(".modal-body").html(body);
+          // Add "Load More" button to load additional Pokemon
+          const loadMoreBtn = $('<button class="btn btn-primary">Load More</button>');
+          loadMoreBtn.click(function() {
+              offset += limit;
+              fetchPokemon();
+          });
+          loadMoreBtn.appendTo($('.container'));
+      });
+  }
+
+  // Fetch individual Pokemon data
+  function fetchPokemonData(url) {
+      return $.ajax({
+          url: url,
+          method: 'GET'
+      });
+  }
+
+  // Display Pokemon details modal
+  function displayPokemonDetails(url) {
+      fetchPokemonData(url).done(function(pokemon) {
+          const pokemonModal = $('#pokemonModal');
+          const pokemonModalLabel = $('#pokemonModalLabel');
+          const pokemonImage = $('#pokemonImage');
+          const pokemonHeight = $('#pokemonHeight');
+          const pokemonWeight = $('#pokemonWeight');
+          const pokemonTypes = $('#pokemonTypes');
+
+          // Update modal content
+          pokemonModalLabel.text(pokemon.name);
+          pokemonImage.attr('src', pokemon.sprites.front_default);
+          pokemonHeight.text(pokemon.height);
+          pokemonWeight.text(pokemon.weight);
+          pokemonTypes.empty();
+          $.each(pokemon.types, function(index, type) {
+              pokemonTypes.append(`<li>${type.type.name}</li>`);
+          });
 
           // Show the modal
-          $("#pokemon-modal").modal("show");
-        });
+          pokemonModal.modal('show');
       });
-    });
-  });
+  }
+
+  // Fetch and display Pokemon on page load
+  fetchPokemon();
 });
